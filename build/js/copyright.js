f45c4ba9be6436dff808fde4a59aa9893e818f48
matthewAbianco <matthewbianco@hotmail.com>
2023-01-12 11:45:25 -0500

    finished

--- build/js/copyright.js
+++ build/js/copyright.js
@@ -4,11 +4,16 @@
 // year.setAttribute("datetime", thisYear)
 // year.textContent = thisYear
 // 1st variation
-let year;
-year = document.getElementById("year");
-let thisYear;
-thisYear = new Date().getFullYear().toString();
-if (year) {
-    year.setAttribute("datetime", thisYear);
-    year.textContent = thisYear;
-}
+// let year: HTMLElement | null
+// year = document.getElementById("year")
+// let thisYear: string
+// thisYear = new Date().getFullYear().toString()
+// if (year) {
+//     year.setAttribute("datetime", thisYear)
+//     year.textContent = thisYear
+// }
+// 2nd variation 
+const year = document.getElementById("year");
+const thisYear = new Date().getFullYear().toString();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
--- src/copyright.ts
+++ src/copyright.ts
@@ -14,10 +14,7 @@
 // }
 
 // 2nd variation 
-const year: HTMLElement | null document.getElementById("year")
-let thisYear: string
-thisYear = new Date().getFullYear().toString()
-if (year) {
-    year.setAttribute("datetime", thisYear)
-    year.textContent = thisYear
-}
+const year = document.getElementById("year") as HTMLSpanElement
+const thisYear: string = new Date().getFullYear().toString()
+year.setAttribute("datetime", thisYear)
+year.textContent = thisYear
